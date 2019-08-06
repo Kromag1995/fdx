cdcfc9b9a4dfa8d7f8296eac0ff78da5281e4d86
Kromag1995 <gonzalof@hotmail.com>
2019-08-06 00:45:24 -0300

    Tutorail Finished

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,23 @@
 """Aca se administran las acciones posibles para admins"""
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 # Register your models here.
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    "Clase para controlar la creacion de choices en la rama de questions"
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    "Clase para modificar como los admins crean Questions"
+    fieldsets = [
+        (None, {'fields' : ['question_text']}),
+        ('Date information', {'fields' : ['pub_date'], 'classes': ['collapse']}),
+        ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter =  ['pub_date']
+    search_fields = ['question_text']
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
--- polls/views.py
+++ polls/views.py
@@ -1,31 +1,37 @@
 """Manejo de los views del sitio web"""
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
-
+from django.views import generic
 
 from .models import Question, Choice
 # Create your views here.
 
-def index(request):
-    """Crea la view principal, una lista de las ultimas 5 preguntas"""
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-def detail(request, question_id):
-    """Devuelve la view para preguntas especificas"""
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
-def results(request, question_id):
-    """Devuelve el resultado para una pregunta especifica"""
-    return HttpResponse("Estas viendo el resultado de {}".format(question_id))
+class IndexView(generic.ListView):
+    'Crea una view generica para index'
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    'Crea una view generica para detail'
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    'Crea una view generica para results'
+    model = Question
+    template_name = 'polls/results.html'
+
 def vote(request, question_id):
     """Devuelve la view para votar una pregunta"""
-    question = get_object_or_404(Question, question_id)
+    question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST('choice'))
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/details.html', {
+        return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice"
         })
